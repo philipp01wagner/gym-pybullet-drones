8210725e7f23cc326fc8c169a1357b9a525d8390
Philipp Wagner <philipp01wagner@gmail.com>
2022-01-27 11:18:14 +0100

    debugging

--- examples/learn_straight_flight.py
+++ examples/learn_straight_flight.py
@@ -19,19 +19,38 @@ It is not meant as a good/effective learning example.
 import time
 import gym
 import numpy as np
+import argparse
 from stable_baselines3 import A2C, PPO, DDPG, SAC
-#from stable_baselines3.ddpg import MlpPolicy
-from stable_baselines3.a2c import MlpPolicy
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.vec_env import VecCheckNan, DummyVecEnv
 from gym_pybullet_drones.envs.single_agent_rl.StraightFlightAviary import StraightFlightAviary
 
+from gym_pybullet_drones.envs.BaseAviary import DroneModel, Physics
 from gym_pybullet_drones.utils.Logger import Logger
-from gym_pybullet_drones.envs.single_agent_rl.TakeoffAviary import TakeoffAviary
 from gym_pybullet_drones.utils.utils import sync
 from gym.envs.registration import register
+from gym_pybullet_drones.utils.utils import sync, str2bool
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Helix flight script using CtrlAviary or VisionAviary and DSLPIDControl')
+    parser.add_argument('--drone',              default="ha",       type=DroneModel,    help='Drone model (default: CF2X)', metavar='', choices=DroneModel)
+    parser.add_argument('--physics',            default="pyb",      type=Physics,       help='Physics updates (default: PYB)', metavar='', choices=Physics)
+    parser.add_argument('--gui',                default=False,       type=str2bool,      help='Whether to use PyBullet GUI (default: True)', metavar='')
+    parser.add_argument('--aggregate',          default=True,       type=str2bool,      help='Whether to aggregate physics steps (default: True)', metavar='')
+    parser.add_argument('--simulation_freq_hz', default=240,        type=int,           help='Simulation frequency in Hz (default: 240)', metavar='')
+    parser.add_argument('--control_freq_hz',    default=48,         type=int,           help='Control frequency in Hz (default: 48)', metavar='')
+    parser.add_argument('--duration_sec',       default=12,         type=int,           help='Duration of the simulation in seconds (default: 5)', metavar='')
+    parser.add_argument('--trajectory',         default=1,          type=int,           help='Trajectory type (default: 1)', metavar='')
+    parser.add_argument('--wind',               default=False,      type=str2bool,      help='Whether to enable wind (default: False)', metavar='')
+    parser.add_argument('--record_video',       default=False,      type=str2bool,      help='Whether to record a video (default: False)', metavar='')
+
+    ARGS = parser.parse_args()
+
+    H = 1.0
+    R = .3
+    INIT_XYZS = np.array([[0, 0, H]])
+    INIT_RPYS = np.array([[0, 0,  0]])
+    AGGR_PHY_STEPS = int(ARGS.simulation_freq_hz/ARGS.control_freq_hz) if ARGS.aggregate else 1
 
     register(
     id='straight-flight-aviary-v0',
@@ -41,7 +60,15 @@ if __name__ == "__main__":
 )
 
     #### Check the environment's spaces ########################
-    env = gym.make("straight-flight-aviary-v0")
+    env = StraightFlightAviary(drone_model=ARGS.drone,
+                         initial_xyzs=INIT_XYZS,
+                         initial_rpys=INIT_RPYS,
+                         physics=ARGS.physics,
+                         freq=ARGS.simulation_freq_hz,
+                         aggregate_phy_steps=AGGR_PHY_STEPS,
+                         gui=ARGS.gui,
+                         record=ARGS.record_video
+                         )
 
     print("[INFO] Action space:", env.action_space)
     print("[INFO] Observation space:", env.observation_space)
@@ -51,12 +78,12 @@ if __name__ == "__main__":
               )
 
     #### Train the model #######################################
-    model = DDPG("MlpPolicy",
+    model = SAC("MlpPolicy",
                     env,
                     verbose=1,
                     tensorboard_log="./a2c_StraightFlight_tensorboard/"
                     )
-    model.learn(total_timesteps=1000000) # Typically not enough
+    model.learn(total_timesteps=100000) # Typically not enough
 
     #### Show (and record a video of) the model's performance ##
     env = StraightFlightAviary(gui=True,
@@ -72,6 +99,7 @@ if __name__ == "__main__":
                                             deterministic=True
                                             )
 
+        print(action)
         obs, reward, done, info = env.step(action)
         logger.log(drone=0,
                    timestamp=i/env.SIM_FREQ,
