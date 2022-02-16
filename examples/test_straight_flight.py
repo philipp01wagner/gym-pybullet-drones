0174388e9e5fd85c4e04a4b0174dd9e2fc83b6ee
Philipp Wagner <philipp01wagner@gmail.com>
2022-02-16 17:54:56 +0100

    test trained policies

---
+++ examples/test_straight_flight.py
@@ -0,0 +1,84 @@
+import time
+import gym
+import numpy as np
+import argparse
+from stable_baselines3 import A2C, PPO, DDPG, SAC
+from stable_baselines3.common.env_checker import check_env
+import pybullet as p
+from gym_pybullet_drones.envs.single_agent_rl.StraightFlightAviary import StraightFlightAviary
+
+from gym_pybullet_drones.envs.BaseAviary import DroneModel, Physics
+from gym_pybullet_drones.utils.Logger import Logger
+from gym_pybullet_drones.utils.utils import sync
+from gym.envs.registration import register
+from gym_pybullet_drones.utils.utils import sync, str2bool#
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Helix flight script using CtrlAviary or VisionAviary and DSLPIDControl')
+    parser.add_argument('--drone',              default="ha",       type=DroneModel,    help='Drone model (default: CF2X)', metavar='', choices=DroneModel)
+    parser.add_argument('--physics',            default="pyb",      type=Physics,       help='Physics updates (default: PYB)', metavar='', choices=Physics)
+    parser.add_argument('--gui',                default=False,       type=str2bool,      help='Whether to use PyBullet GUI (default: True)', metavar='')
+    parser.add_argument('--aggregate',          default=True,       type=str2bool,      help='Whether to aggregate physics steps (default: True)', metavar='')
+    parser.add_argument('--simulation_freq_hz', default=240,        type=int,           help='Simulation frequency in Hz (default: 240)', metavar='')
+    parser.add_argument('--control_freq_hz',    default=48,         type=int,           help='Control frequency in Hz (default: 48)', metavar='')
+    parser.add_argument('--duration_sec',       default=12,         type=int,           help='Duration of the simulation in seconds (default: 5)', metavar='')
+    parser.add_argument('--trajectory',         default=1,          type=int,           help='Trajectory type (default: 1)', metavar='')
+    parser.add_argument('--wind',               default=False,      type=str2bool,      help='Whether to enable wind (default: False)', metavar='')
+    parser.add_argument('--record_video',       default=False,      type=str2bool,      help='Whether to record a video (default: False)', metavar='')
+
+    ARGS = parser.parse_args()
+
+    H = 1.0
+    R = .3
+    INIT_XYZS = np.array([[0, 0, H]])
+    INIT_RPYS = np.array([[0, 0,  0]])
+    AGGR_PHY_STEPS = int(ARGS.simulation_freq_hz/ARGS.control_freq_hz) if ARGS.aggregate else 1
+
+    env = StraightFlightAviary(gui=True,
+                        record=False,
+                        initial_xyzs=INIT_XYZS,
+                        initial_rpys=INIT_RPYS,
+                        physics=ARGS.physics,
+                        freq=ARGS.simulation_freq_hz,
+                        aggregate_phy_steps=AGGR_PHY_STEPS
+                        )
+
+    PYB_CLIENT = env.getPyBulletClient()
+    p.setGravity(0,0,0)
+    """model = A2C("MlpPolicy",
+                    env,
+                    verbose=1
+                    )"""
+    model = A2C.load("a2c_model2")
+
+    
+    logger = Logger(logging_freq_hz=int(env.SIM_FREQ/env.AGGR_PHY_STEPS),
+                    num_drones=1
+                    )
+    #obs = env.reset()
+    action = np.array([0,0])
+    start = time.time()
+    for i in range(20*env.SIM_FREQ):
+        obs, reward, done, info = env.step(100*action)
+        logger.log(drone=0,
+                   timestamp=i/env.SIM_FREQ,
+                   state=np.hstack([obs[0:3], np.zeros(4), obs[3:15],  np.resize(action, (2))]),
+                   control=np.zeros(12)
+                   )
+        if i%env.SIM_FREQ == 0:
+            env.render()
+            print(done)
+        sync(i, start, env.TIMESTEP)
+        if done:
+            print("DONE")
+            obs = env.reset()
+        action, _states = model.predict(obs,
+                                            deterministic=True
+                                            )
+
+        print(action)
+        
+    env.close()
+    logger.plot()
