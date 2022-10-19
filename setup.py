f5d339bde7ade416887c808da9e03c0e5ef88f99
Philipp Wagner <philipp01wagner@gmail.com>
2022-10-19 08:38:52 +0200

    update setup

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='gym_pybullet_drones',
         'gym==0.21.0',
         'pybullet',
         'stable_baselines3==1.6.0',
-        'ray[rllib]'
+        'ray[rllib]',
+        'tensorboard'
         ]
 )
